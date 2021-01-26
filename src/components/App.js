bb8a7fdf7181d9367dd5fb3a9f5b9461fdd3c525
Egri Viktor <egrivik@gmail.com>
2021-01-26 08:39:40 +0100

    Async-await-for-API-call

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-onSearchSubmit(term) {
-  axios.get('https://api.unsplash.com/search/photos', {
-    params: { query: term },
-    headers: {
-      Authorization: 'Client-ID yT7aNUEVqYJZg5fsrNjB-E8FgmYCHZElyYmV6dHRtx0'
-    }
-  });
-}
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID yT7aNUEVqYJZg5fsrNjB-E8FgmYCHZElyYmV6dHRtx0'
+      }
+    });
+
+    console.log(response.data.results);
+  }
 
   render() {
     return (
